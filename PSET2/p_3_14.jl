09d36a3a5c0b1900e3816f2619f6f07eb93afe87
Adam Drozynski <adam@Adams-MacBook-Pro.local>
2025-10-17 09:41:03 -0500

    314 up to d

--- PSET2/p_3_14.jl
+++ PSET2/p_3_14.jl
@@ -21,7 +21,7 @@ function B(x::AbstractVector{<:Real}, Y::AbstractVector{<:Real}, X::AbstractVect
 end
 
 function B_var(x::Real, Y::AbstractVector{<:Real}, X::AbstractVector{<:Real})
-    return var(Y[X .== x])
+    return var(Y[X .== x]) / mean(X .== x)
 end
 
 function B_var(x::AbstractVector{<:Real}, Y::AbstractVector{<:Real}, X::AbstractVector{<:Real})
@@ -44,10 +44,15 @@ function ∇σ_hat(B::AbstractVector{<:Real})
     return [(B[i] - mean(B)) / (length(B) * σ_hat(B)) for i in eachindex(B)]
 end
 
+function σ_hat_var(x::AbstractVector{<:Real}, Y::AbstractVector{<:Real}, X::AbstractVector{<:Real}; 
+    B::AbstractVector{<:Real} = B(x, Y, X), 
+    B_var::AbstractMatrix{<:Real} = B_var(x, Y, X))
+    return ∇σ_hat(B)' * B_var * ∇σ_hat(B)
+end
 
 # Number of MC simulations
-M = 10
-
+M = 1000
+α = 0.05
 data = DataFrame(load("cps.dta"))
 U = data.wage
 U = collect(skipmissing(U))
@@ -68,16 +73,77 @@ state_rank_counts = countmap(data_10.state_rank)
 F_X = DiscreteNonParametric(collect(keys(state_rank_counts)), values(state_rank_counts) ./ sum(values(state_rank_counts)))
 
 
-σ_values = collect(LinRange(0.0, 2.0, 100))
+σ_values = collect(LinRange(0.0, 3.0, 50))
 N_values = [Int64(round(length(U)/ 2)), Int64(round(length(U))), Int64(round(length(U) * 2))]
-@time for m in 1:M
-    sample_X = rand(F_X, N_values[1])
-    sample_U = rand(U, N_values[1])
+S_n_values = zeros(M, length(σ_values), length(N_values))
+S_n_vars = zeros(M, length(σ_values), length(N_values))
+S_n_CI_low = zeros(M, length(σ_values), length(N_values))
+S_n_CI_high = zeros(M, length(σ_values), length(N_values))
 
-    sample_Y = (-1).^(sample_X) * σ_values[50] .+ sample_U
+for i in eachindex(σ_values)
+    for j in eachindex(N_values)
+        for m in 1:M
+            sample_X = rand(F_X, N_values[j])
+            sample_U = rand(U, N_values[j])
 
-    B_n = B(collect(1:10), sample_Y, sample_X)
-    S_n = σ_hat(B_n)
+            sample_Y = (-1).^(sample_X) * σ_values[i] .+ sample_U
 
-    display(S_n)
+            B_n = B(collect(1:10), sample_Y, sample_X)
+            B_n_vars = B_var(collect(1:10), sample_Y, sample_X)
+            S_n_values[m, i, j] = σ_hat(B_n)
+            S_n_vars[m, i, j] = σ_hat_var(collect(1:10), sample_Y, sample_X, B=B_n, B_var=B_n_vars)
+
+            S_n_CI_low[m, i, j] = S_n_values[m, i, j] - Φ_inv(1 - α/2) * sqrt(S_n_vars[m, i, j] / N_values[j])
+            S_n_CI_high[m, i, j] = S_n_values[m, i, j] + Φ_inv(1 - α/2) * sqrt(S_n_vars[m, i, j] / N_values[j])
+        end
+    end
 end
+
+mean_S_n = mean(S_n_values, dims=1) 
+mean_S_n = dropdims(mean_S_n; dims=1)
+mean_S_n_vars = mean(S_n_vars, dims=1)
+mean_S_n_vars = dropdims(mean_S_n_vars; dims=1)
+mean_S_n_CI_low = mean(S_n_CI_low, dims=1)
+mean_S_n_CI_low = dropdims(mean_S_n_CI_low; dims=1)
+mean_S_n_CI_high = mean(S_n_CI_high, dims=1)
+mean_S_n_CI_high = dropdims(mean_S_n_CI_high; dims=1)
+
+plt = plot()
+for j in 1:length(N_values)
+    plot!(plt, σ_values, mean_S_n[:,j], ribbon=(mean_S_n_CI_high[:,j] - mean_S_n[:,j], mean_S_n[:,j] - mean_S_n_CI_low[:,j]), fillalpha=0.2, label = "N = $(N_values[j])")
+end
+plot!(σ_values, σ_values, label = "True")
+xlabel!(plt, "σ")
+ylabel!(plt, "Mean Sₙ")
+title!(plt, "Mean Sₙ by σ for different N")
+savefig(plt, "PSET2/mean_S_n.pdf")
+
+plt = plot()
+for j in 1:length(N_values)
+    plot!(plt, σ_values, mean_S_n_vars[:,j], label = "N = $(N_values[j])")
+end
+xlabel!(plt, "σ")
+ylabel!(plt, "Mean Sₙ")
+title!(plt, "Mean Sₙ by σ for different N")
+savefig(plt, "PSET2/mean_S_n_vars.pdf")
+
+
+display(S_n_CI_low[:,end,end] .<= σ_values[end])
+display(σ_values[end] .<= S_n_CI_high[:,end,end])
+
+display(mean(S_n_CI_low[:,end,end] .<= σ_values[end]))
+display(mean(σ_values[end] .<= S_n_CI_high[:,end,end]))
+
+
+true_in_CI = [mean((S_n_CI_low[:,i,j] .<= σ_values[i]) .&& (σ_values[i] .<= S_n_CI_high[:,i,j])) for i in eachindex(σ_values), j in eachindex(N_values)]
+display(true_in_CI)
+
+plt = plot()
+for j in 1:length(N_values)
+    plot!(plt, σ_values, true_in_CI[:,j], label = "N = $(N_values[j])")
+end
+xlabel!(plt, "σ")
+ylabel!(plt, "True in CI")
+title!(plt, "True in CI by σ for different N")
+hline!([[1 - α]], label = "1 - α", color = :black, style=:dash)
+savefig(plt, "PSET2/true_in_CI.pdf")
