523640437f62e07317d2c31547391aeaae7544df
Adam Drozynski <adam@Adams-MacBook-Pro.local>
2025-10-18 16:04:10 -0500

    Done, just gotta rerun it all

--- PSET2/p_3_14.jl
+++ PSET2/p_3_14.jl
@@ -1,7 +1,7 @@
 using Pkg
 Pkg.activate(".")
 
-using LinearAlgebra, StatsBase, Statistics, Plots, StatsPlots, Random, Distributions, DataFrames, StatFiles
+using LinearAlgebra, StatsBase, Statistics, Plots, StatsPlots, Random, Distributions, DataFrames, StatFiles, LaTeXStrings
 
 ##### Key statistical functions
 function Φ(x)
@@ -64,9 +64,19 @@ function σ2_hat_var(x::AbstractVector{<:Real}, Y::AbstractVector{<:Real}, X::Ab
     return ∇σ2_hat(B)' * B_var * ∇σ2_hat(B)
 end
 
+function tilde_σ2_hat(x::AbstractVector{<:Real}, Y::AbstractVector{<:Real}, X::AbstractVector{<:Real})
+    B_n = B(x, Y, X)
+    N = [length(Y[X .== x[i]]) for i in eachindex(x)]
+    σ2_hat_n = σ2_hat(B_n)
+
+    S2_n = [sum( Int64.(X .== x[i]) .* (Y .- B_n[i]).^2 ) / (N[i] - 1) for i in eachindex(x)]
+    return σ2_hat_n - (length(B_n) - 1) / (length(B_n)^2) * sum(S2_n ./ N)
+end
+
 # Number of MC simulations
-M = 1000
+M = 10
 α = 0.05
+states = 10
 data = DataFrame(load("cps.dta"))
 U = data.wage
 U = collect(skipmissing(U))
@@ -75,14 +85,14 @@ U = Float64.(U)
 state = data.state
 
 state_counts = countmap(state)
-top_states = sort(collect(keys(state_counts)), by=x->-state_counts[x])[1:10]
-data_10 = data[in.(data.state, Ref(top_states)), :]
+top_states = sort(collect(keys(state_counts)), by=x->-state_counts[x])[1:states]
+data = data[in.(data.state, Ref(top_states)), :]
 
-state_freqs = countmap(data_10.state)
+state_freqs = countmap(data.state)
 sorted_states = sort(collect(keys(state_freqs)), by=x -> -state_freqs[x])
 state_to_rank = Dict(s => r for (r, s) in enumerate(sorted_states))
-data_10.state_rank = [state_to_rank[s] for s in data_10.state]
-state_rank_counts = countmap(data_10.state_rank)
+data.state_rank = [state_to_rank[s] for s in data.state]
+state_rank_counts = countmap(data.state_rank)
 
 F_X = DiscreteNonParametric(collect(keys(state_rank_counts)), values(state_rank_counts) ./ sum(values(state_rank_counts)))
 
@@ -100,6 +110,11 @@ V_n_vars = zeros(M, length(σ_values), length(N_values))
 V_n_CI_low = zeros(M, length(σ_values), length(N_values))
 V_n_CI_high = zeros(M, length(σ_values), length(N_values))
 
+tilde_V_n_values = zeros(M, length(σ_values), length(N_values))
+tilde_V_n_vars = zeros(M, length(σ_values), length(N_values))
+tilde_V_n_CI_low = zeros(M, length(σ_values), length(N_values))
+tilde_V_n_CI_high = zeros(M, length(σ_values), length(N_values))
+
 for i in eachindex(σ_values)
     for j in eachindex(N_values)
         for m in 1:M
@@ -108,18 +123,23 @@ for i in eachindex(σ_values)
 
             sample_Y = (-1).^(sample_X) * σ_values[i] .+ sample_U
 
-            B_n = B(collect(1:10), sample_Y, sample_X)
-            B_n_vars = B_var(collect(1:10), sample_Y, sample_X)
+            B_n = B(collect(1:states), sample_Y, sample_X)
+            B_n_vars = B_var(collect(1:states), sample_Y, sample_X)
 
             S_n_values[m, i, j] = σ_hat(B_n)
-            S_n_vars[m, i, j] = σ_hat_var(collect(1:10), sample_Y, sample_X, B=B_n, B_var=B_n_vars)
+            S_n_vars[m, i, j] = σ_hat_var(collect(1:states), sample_Y, sample_X, B=B_n, B_var=B_n_vars)
             S_n_CI_low[m, i, j] = S_n_values[m, i, j] - Φ_inv(1 - α/2) * sqrt(S_n_vars[m, i, j] / N_values[j])
             S_n_CI_high[m, i, j] = S_n_values[m, i, j] + Φ_inv(1 - α/2) * sqrt(S_n_vars[m, i, j] / N_values[j])
 
             V_n_values[m, i, j] = σ2_hat(B_n)
-            V_n_vars[m, i, j] = σ2_hat_var(collect(1:10), sample_Y, sample_X, B=B_n, B_var=B_n_vars)
+            V_n_vars[m, i, j] = σ2_hat_var(collect(1:states), sample_Y, sample_X, B=B_n, B_var=B_n_vars)
             V_n_CI_low[m, i, j] = V_n_values[m, i, j] - Φ_inv(1 - α/2) * sqrt(V_n_vars[m, i, j] / N_values[j])
             V_n_CI_high[m, i, j] = V_n_values[m, i, j] + Φ_inv(1 - α/2) * sqrt(V_n_vars[m, i, j] / N_values[j])
+
+            tilde_V_n_values[m, i, j] = tilde_σ2_hat(collect(1:states), sample_Y, sample_X)
+            tilde_V_n_vars[m, i, j] = V_n_vars[m, i, j]
+            tilde_V_n_CI_low[m, i, j] = tilde_V_n_values[m, i, j] - Φ_inv(1 - α/2) * sqrt(tilde_V_n_vars[m, i, j] / N_values[j])
+            tilde_V_n_CI_high[m, i, j] = tilde_V_n_values[m, i, j] + Φ_inv(1 - α/2) * sqrt(tilde_V_n_vars[m, i, j] / N_values[j])
         end
     end
 end
@@ -142,25 +162,45 @@ mean_V_n_CI_low = dropdims(mean_V_n_CI_low; dims=1)
 mean_V_n_CI_high = mean(V_n_CI_high, dims=1)
 mean_V_n_CI_high = dropdims(mean_V_n_CI_high; dims=1)
 
+mean_tilde_V_n = mean(tilde_V_n_values, dims=1)
+mean_tilde_V_n = dropdims(mean_tilde_V_n; dims=1)
+mean_tilde_V_n_vars = mean(tilde_V_n_vars, dims=1)
+mean_tilde_V_n_vars = dropdims(mean_tilde_V_n_vars; dims=1)
+mean_tilde_V_n_CI_low = mean(tilde_V_n_CI_low, dims=1)
+mean_tilde_V_n_CI_low = dropdims(mean_tilde_V_n_CI_low; dims=1)
+mean_tilde_V_n_CI_high = mean(tilde_V_n_CI_high, dims=1)
+mean_tilde_V_n_CI_high = dropdims(mean_tilde_V_n_CI_high; dims=1)
+
+#### Estimator's means and confidence bands
+
 plt = plot()
 for j in 1:length(N_values)
     plot!(plt, σ_values, mean_S_n[:,j], ribbon=(mean_S_n_CI_high[:,j] - mean_S_n[:,j], mean_S_n[:,j] - mean_S_n_CI_low[:,j]), fillalpha=0.2, label = "N = $(N_values[j])")
 end
 plot!(σ_values, σ_values, label = "True")
-xlabel!(plt, "σ")
-ylabel!(plt, "Mean Sₙ")
-title!(plt, "Mean Sₙ by σ for different N")
-savefig(plt, "PSET2/mean_S_n.pdf")
+xlabel!(plt, L"$\sigma$")
+title!(plt, L"$S_n$ by $\sigma$ for different N")
+savefig(plt, "PSET2/mean_S_n_$(states).pdf")
 
 plt = plot()
 for j in 1:length(N_values)
     plot!(plt, σ_values.^2, mean_V_n[:,j], ribbon=(mean_V_n_CI_high[:,j] - mean_V_n[:,j], mean_V_n[:,j] - mean_V_n_CI_low[:,j]), fillalpha=0.2, label = "N = $(N_values[j])")
 end
 plot!(σ_values.^2, σ_values.^2, label = "True")
-xlabel!(plt, "σ")
-ylabel!(plt, "Mean Vₙ")
-title!(plt, "Mean Vₙ by σ for different N")
-savefig(plt, "PSET2/mean_V_n.pdf")
+xlabel!(plt, L"$\sigma^2$")
+title!(plt, L"$S_n^2$ by $\sigma^2$ for different N")
+savefig(plt, "PSET2/mean_V_n_$(states).pdf")
+
+plt = plot()
+for j in 1:length(N_values)
+    plot!(plt, σ_values.^2, mean_tilde_V_n[:,j], ribbon = (mean_tilde_V_n_CI_high[:,j] - mean_tilde_V_n[:,j], mean_tilde_V_n[:,j] - mean_tilde_V_n_CI_low[:,j]), fillalpha=0.2, label = "N = $(N_values[j])")
+end
+plot!(σ_values.^2, σ_values.^2, label = "True")
+xlabel!(plt, L"$\sigma^2$")
+title!(plt, L"$\tilde{S}_n^2$ by $\sigma^2$ for different N")
+savefig(plt, "PSET2/mean_tilde_V_n_$(states).pdf")
+
+##### CIs Test
 
 true_in_CI = [mean((S_n_CI_low[:,i,j] .<= σ_values[i]) .&& (σ_values[i] .<= S_n_CI_high[:,i,j])) for i in eachindex(σ_values), j in eachindex(N_values)]
 display(true_in_CI)
@@ -169,11 +209,10 @@ plt = plot()
 for j in 1:length(N_values)
     plot!(plt, σ_values, true_in_CI[:,j], label = "N = $(N_values[j])")
 end
-xlabel!(plt, "σ")
-ylabel!(plt, "True in CI")
-title!(plt, "True in CI by σ for different N")
-hline!([1 - α], label = "1 - α", color = :black, style=:dash)
-savefig(plt, "PSET2/true_in_CI.pdf")
+xlabel!(plt, L"$\sigma$")
+title!(plt, L"True $\sigma$ in CI by $\sigma$ for different N")
+hline!([1 - α], label = L"1 - \alpha", color = :black, style=:dash)
+savefig(plt, "PSET2/true_in_CI_$(states).pdf")
 
 true_in_CI_V = [mean((V_n_CI_low[:,i,j] .<= σ_values[i].^2) .&& (σ_values[i].^2 .<= V_n_CI_high[:,i,j])) for i in eachindex(σ_values), j in eachindex(N_values)]
 display(true_in_CI_V)
@@ -182,8 +221,19 @@ plt = plot()
 for j in 1:length(N_values)
     plot!(plt, σ_values.^2, true_in_CI_V[:,j], label = "N = $(N_values[j])")
 end
+xlabel!(plt, L"$\sigma^2$")
+title!(plt, L"True $\sigma^2$ in CI by $\sigma^2$ for different N")
+hline!([1 - α], label = L"1 - \alpha", color = :black, style=:dash)
+savefig(plt, "PSET2/true_in_CI_V_$(states).pdf")
+
+true_in_CI_tilde_V = [mean((tilde_V_n_CI_low[:,i,j] .<= σ_values[i].^2) .&& (σ_values[i].^2 .<= tilde_V_n_CI_high[:,i,j])) for i in eachindex(σ_values), j in eachindex(N_values)]
+display(true_in_CI_tilde_V)
+
+plt = plot()
+for j in 1:length(N_values)
+    plot!(plt, σ_values.^2, true_in_CI_tilde_V[:,j], label = "N = $(N_values[j])")
+end
 xlabel!(plt, "σ")
-ylabel!(plt, "True in CI")
-title!(plt, "True in CI by σ for different N")
-hline!([1 - α], label = "1 - α", color = :black, style=:dash)
-savefig(plt, "PSET2/true_in_CI_V.pdf")
+title!(plt, L"True $\sigma^2$ in CI by $\sigma^2$ for different N")
+hline!([1 - α], label = L"1 - \alpha", color = :black, style=:dash)
+savefig(plt, "PSET2/true_in_CI_tilde_V_$(states).pdf")
