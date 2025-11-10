d2b945942c9e6949d540cbd839e7010eed0ac34a
Adam Drozynski <adam@Adams-MacBook-Pro.local>
2025-11-10 13:44:19 -0600

    Almost done with it, now its a bit boring to figure this last thoing out

--- PSET4/p_6_13.jl
+++ PSET4/p_6_13.jl
@@ -29,17 +29,31 @@ function cv_χ2(α::Real, df::Integer)
 end
 
 function V_OLS_HC0(X::Matrix{<:Real}, Y::Vector{<:Real}; B::Vector{<:Real} = OLS(X, Y))
+    inv_X_X = inv(X' * X)
     N = length(Y)
     U = Y - X * B
     U_dg = diagm(U.^2)
-    return N * inv(X' * X) * X' * U_dg * X * inv(X' * X)
+    return N * inv_X_X * X' * U_dg * X * inv_X_X
 end
 
 function V_OLS_HC1(X::Matrix{<:Real}, Y::Vector{<:Real}; B::Vector{<:Real} = OLS(X, Y))
+    inv_X_X = inv(X' * X)
     N = length(Y)
     U = Y - X * B
     U_dg = diagm(U.^2)
-    return (N^2 / (N - size(X, 2))) * inv(X' * X) * X' * U_dg * X * inv(X' * X)
+    return (N^2 / (N - size(X, 2))) * inv_X_X * X' * U_dg * X * inv_X_X
+end
+
+function leverage(X::Matrix{<:Real}; inv_X_X::Matrix{<:Real} = inv(X' * X))
+    return diag(X * inv_X_X * X')
+end
+
+function V_OLS_HC3(X::Matrix{<:Real}, Y::Vector{<:Real}; B::Vector{<:Real} = OLS(X, Y))
+    inv_X_X = inv(X' * X)
+    N = length(Y)
+    U = Y - X * B
+    U_dg = diagm(U.^2 ./ (1 .- leverage(X; inv_X_X=inv_X_X)).^2)
+    return N * inv_X_X * X' * U_dg * X * inv_X_X
 end
 
 function V_OLS_HOM(X::Matrix{<:Real}, Y::Vector{<:Real}; B::Vector{<:Real} = OLS(X, Y))
@@ -55,8 +69,16 @@ function T_test(α::Real, β_0::Real, B::Real, σ::Real)
     return abs(T_stat(β_0, B, σ)) > cv_normal(α)
 end
 
-function P_value_t(β_0::Real, B::Real, σ::Real)
+function T_test_t(α::Real, β_0::Real, B::Real, σ::Real, df::Integer)
+    return abs(T_stat(β_0, B, σ)) > cv_t(α, df)
+end
+
+function P_value_normal(β_0::Real, B::Real, σ::Real)
     return 2 * (1 - Φ(abs(T_stat(β_0, B, σ))))
+end
+
+function P_value_t(β_0::Real, B::Real, σ::Real, df::Integer)
+    return 2 * (1 - cdf(TDist(df), abs(T_stat(β_0, B, σ))))
 end
 
 ##### Some helper functions
@@ -65,12 +87,12 @@ function create_fe_matrix(X::AbstractVector{<:Any})
     X_base = X_vals[1]
     X_fe = zeros(length(X), length(X_vals) - 1)
     for i in 2:length(X_vals)
-        X_fe[:, i-1] = Int.(X .== X_vals[i])
+        X_fe[:, i-1] = [Int(X[j] == X_vals[i]) for j in eachindex(X)]
     end
     return X_fe, X_base
 end
 
-α = 0.05
+α = 0.1
 
 data = DataFrame(load("cps.dta"))
 
@@ -79,47 +101,116 @@ data.sex = Int.(data.sex .== "Female")
 data.age = Int.(data.age)
 
 
-#### (a)
+# #### (a)
+# display("Part (a)")
+# data_filtered = data[data.state .== "Illinois", :]
+# data_filtered = data_filtered[data_filtered.age .== 35, :]
+# data_filtered = data_filtered[data_filtered.educ .== "High school diploma or equivalent", :]
 
-data_filtered = data[data.state .== "Illinois", :]
-data_filtered = data_filtered[data_filtered.age .== 35, :]
-data_filtered = data_filtered[data_filtered.educ .== "High school diploma or equivalent", :]
+# Y = Float64.(log.(data_filtered.wage))
+# X = [data_filtered.sex ones(size(data_filtered, 1))]
 
-Y = Float64.(log.(data_filtered.wage))
-X = [data_filtered.sex ones(size(data_filtered, 1))]
+# display(length(Y))
 
-display(length(Y))
+# B = OLS(X, Y)
+# display(B)
 
-B = OLS(X, Y)
-display(B)
+# V_HOM = V_OLS_HOM(X, Y, B=B)
+# V_HC0 = V_OLS_HC0(X, Y, B=B)
+# V_HC1 = V_OLS_HC1(X, Y, B=B)
 
-V_HOM = V_OLS_HOM(X, Y, B=B)
-S_HOM = sqrt.(diag(V_HOM) ./ length(Y))
-display(S_HOM)
+# ci_HOM = (B[1] - cv_normal(α) * sqrt(V_HOM[1,1] / length(Y)), B[1] + cv_normal(α) * sqrt(V_HOM[1,1] / length(Y)))
+# ci_HC0 = (B[1] - cv_normal(α) * sqrt(V_HC0[1,1] / length(Y)), B[1] + cv_normal(α) * sqrt(V_HC0[1,1] / length(Y)))
+# ci_HC1 = (B[1] - cv_normal(α) * sqrt(V_HC1[1,1] / length(Y)), B[1] + cv_normal(α) * sqrt(V_HC1[1,1] / length(Y)))
+# ci_T = (B[1] - cv_t(α, length(Y) - size(X, 2)) * sqrt(V_HOM[1,1] / length(Y)), B[1] + cv_t(α, length(Y) - size(X, 2)) * sqrt(V_HOM[1,1] / length(Y)))
 
-V_HC0 = V_OLS_HC0(X, Y, B=B)
-S_HC0 = sqrt.(diag(V_HC0) ./ length(Y))
-display(S_HC0)
+# display(ci_HOM)
+# display(ci_HC1)
+# display(ci_T)
 
-V_HC1 = V_OLS_HC1(X, Y, B=B)
-S_HC1 = sqrt.(diag(V_HC1) ./ length(Y))
-display(S_HC1)
-ci_HOM = (B[1] - cv_normal(α) * sqrt(V_HOM[1,1] / length(Y)), B[1] + cv_normal(α) * sqrt(V_HOM[1,1] / length(Y)))
-ci_HC0 = (B[1] - cv_normal(α) * sqrt(V_HC0[1,1] / length(Y)), B[1] + cv_normal(α) * sqrt(V_HC0[1,1] / length(Y)))
-ci_HC1 = (B[1] - cv_normal(α) * sqrt(V_HC1[1,1] / length(Y)), B[1] + cv_normal(α) * sqrt(V_HC1[1,1] / length(Y)))
-ci_T = (B[1] - cv_t(α, length(Y) - size(X, 2)) * sqrt(V_HOM[1,1] / length(Y)), B[1] + cv_t(α, length(Y) - size(X, 2)) * sqrt(V_HOM[1,1] / length(Y)))
+# #### (b)
+# display("Part (b)")
+# Y = Float64.(log.(data.wage))
+# X_educ, X_educ_base = create_fe_matrix(data.educ)
+# X_state, X_state_base = create_fe_matrix(data.state)
+# X = [data.sex data.age data.age.^2 X_educ X_state ones(size(data, 1))]
+# display(size(Y))
+# display(size(X))
+# display(X_educ_base)
+# display(X_state_base)
 
-display(ci_HOM)
-display(ci_HC0)
-display(ci_HC1)
-display(ci_T)
+# B = OLS(X, Y)
+# display(B[1:3])
+
+# V_HOM = V_OLS_HOM(X, Y, B=B)
+# V_HC1 = V_OLS_HC1(X, Y, B=B)
+# V_HC3 = V_OLS_HC3(X, Y, B=B)
+
+# se_sex_HOM = sqrt(V_HOM[1,1] / length(Y))
+# se_sex_HC1 = sqrt(V_HC1[1,1] / length(Y))
+# se_sex_HC3 = sqrt(V_HC3[1,1] / length(Y))
+
+# # HOM & T
+# display("HOM & T")
+# display(se_sex_HOM)
+# display(T_stat(0, B[1], se_sex_HOM))
+# display(P_value_t(0, B[1], se_sex_HOM, length(Y) - size(X, 2)))
+
+# # HC1 & Normal
+# display("HC1 & Normal")
+# display(se_sex_HC1)
+# display(T_stat(0, B[1], se_sex_HC1))
+# display(P_value_normal(0, B[1], se_sex_HC1))
+
+# # HC3 & Normal
+# display("HC3 & Normal")
+# display(se_sex_HC3)
+# display(T_stat(0, B[1], se_sex_HC3))
+# display(P_value_normal(0, B[1], se_sex_HC3))
+
+# #### (c)
+# display("Part (c)")
+# data_filtered = data[data.state .== "Illinois", :]
+# data_filtered = data_filtered[data_filtered.educ .== "High school diploma or equivalent", :]
+# Y = Float64.(log.(data_filtered.wage))
+# X = [data_filtered.sex data_filtered.age data_filtered.age.^2 ones(size(data_filtered, 1))]
+# display(size(Y))
+# display(size(X))
+
+# B = OLS(X, Y)
+# display(B[1:3])
+
+# V_HOM = V_OLS_HOM(X, Y, B=B)
+# V_HC1 = V_OLS_HC1(X, Y, B=B)
+# V_HC3 = V_OLS_HC3(X, Y, B=B)
+
+# se_sex_HOM = sqrt(V_HOM[1,1] / length(Y))
+# se_sex_HC1 = sqrt(V_HC1[1,1] / length(Y))
+# se_sex_HC3 = sqrt(V_HC3[1,1] / length(Y))
+
+# # HC1 & Normal
+# display("HC1 & Normal")
+# display(se_sex_HC1)
+# display(T_stat(0, B[1], se_sex_HC1))
+# display(P_value_normal(0, B[1], se_sex_HC1))
 
 
-#### (b)
+#### (d)
+display("Part (d)")
 Y = Float64.(log.(data.wage))
 X_educ, X_educ_base = create_fe_matrix(data.educ)
-X_state, X_state_base = create_fe_matrix(data.state)
-X = [data.sex data.age data.age.^2 X_educ X_state ones(size(data, 1))]
+sex_state = collect(zip(data.sex, data.state))
+X_state_sex, X_state_sex_base = create_fe_matrix(sex_state)
+X = [X_state_sex data.age data.age.^2 X_educ ones(size(data, 1))]
+display(X_state_sex_base)
+
+r = size(X_state_sex, 2)
+B = OLS(X, Y)
+V_HC1 = V_OLS_HC1(X, Y, B=B)
+display(r)
+
+display(B[1:(r+1)])
+exit()
 display(size(Y))
 display(size(X))
 display(X_educ_base)
@@ -129,25 +220,27 @@ B = OLS(X, Y)
 display(B[1:3])
 
 V_HOM = V_OLS_HOM(X, Y, B=B)
-V_HC0 = V_OLS_HC0(X, Y, B=B)
 V_HC1 = V_OLS_HC1(X, Y, B=B)
+V_HC3 = V_OLS_HC3(X, Y, B=B)
 
 se_sex_HOM = sqrt(V_HOM[1,1] / length(Y))
-se_sex_HC0 = sqrt(V_HC0[1,1] / length(Y))
 se_sex_HC1 = sqrt(V_HC1[1,1] / length(Y))
+se_sex_HC3 = sqrt(V_HC3[1,1] / length(Y))
 
+# HOM & T
+display("HOM & T")
 display(se_sex_HOM)
-display(se_sex_HC0)
+display(T_stat(0, B[1], se_sex_HOM))
+display(P_value_t(0, B[1], se_sex_HOM, length(Y) - size(X, 2)))
+
+# HC1 & Normal
+display("HC1 & Normal")
 display(se_sex_HC1)
+display(T_stat(0, B[1], se_sex_HC1))
+display(P_value_normal(0, B[1], se_sex_HC1))
 
-ci_sex_HOM = (B[1] - cv_normal(α) * se_sex_HOM, B[1] + cv_normal(α) * se_sex_HOM)
-ci_sex_HC0 = (B[1] - cv_normal(α) * se_sex_HC0, B[1] + cv_normal(α) * se_sex_HC0)
-ci_sex_HC1 = (B[1] - cv_normal(α) * se_sex_HC1, B[1] + cv_normal(α) * se_sex_HC1)
-
-display(P_value_t(0, B[1], se_sex_HOM))
-display(P_value_t(0, B[1], se_sex_HC0))
-display(P_value_t(0, B[1], se_sex_HC1))
-
-display(ci_sex_HOM)
-display(ci_sex_HC0)
-display(ci_sex_HC1)
+# HC3 & Normal
+display("HC3 & Normal")
+display(se_sex_HC3)
+display(T_stat(0, B[1], se_sex_HC3))
+display(P_value_normal(0, B[1], se_sex_HC3))
